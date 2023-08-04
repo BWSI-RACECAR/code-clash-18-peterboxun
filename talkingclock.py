a2054727e942c18c627b552f76d2321cb6082932
peterboxun <128928752+peterboxun@users.noreply.github.com>
2023-08-04 09:30:15 -0400

    Add files via upload

---
+++ talkingclock.py
@@ -0,0 +1,56 @@
+"""
+Copyright MIT BWSI Autonomous RACECAR Course
+MIT License
+Summer 2023
+
+Code Clash #18 - Talking Clock (talkingclock.py)
+
+
+Author: Andrew Scott White
+
+Difficulty Level: 8/10
+
+Prompt: I don’t want to be late for the BWSI Grand Prix, so I want
+to program my phone to update me on the time. Can you help me make 
+a Talking Clock? I need a script that takes an input 24-hour time 
+(00:00 - 23:59) and translates it into words. Please format the input 
+as ‘##:##’ and include am/pm in the output string. Thanks!
+
+Test Cases:
+Input: 12:00  Output: It's twelve pm
+
+Input: 23:59  Output: It's eleven fifty nine pm
+
+Input: 12:05  Output: It's twelve oh five pm
+"""
+
+
+
+
+    # This will convert military hours to regular hours, and determine AM vs PM
+class Solution:    
+    def ClockTalker(self, input_time):
+            #type input_time: string
+            #return type: string
+            
+            #TODO: Write code below to return a string with the solution to the prompt.
+            num = input_time.split(':')
+            hours = []
+            minutes = []
+            hours.append(num[0])
+            minutes.append(num[1])
+            a = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+            b = ["twenty", "thirty", "forty", "fifty"]
+            if hours[0] < 12 and minutes[0] < 10:
+                print(a[hours[0]-1],a[minutes[0]-1])
+                
+
+def main():
+     str1=input()
+     tc1= Solution()
+     ans=tc1.ClockTalker(str1)
+     print(ans)
+    
+if __name__ == '__main__': 
+    main()
+        
